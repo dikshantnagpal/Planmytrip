fa5cc3c29ba34c0196e00a0dde24de9757eca11d
vikram <vk14133m02059@gmail.com>
2023-01-18 20:57:17 +0530

    Added Navbar & Footer

---
+++ planmytrip/src/Components/Footer.jsx
@@ -0,0 +1,175 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { spam, useNavigate } from "react-router-dom";
+import "../App.css";
+
+const Footer = () => {
+  const navigate = useNavigate();
+  return (
+    <div style={{ backgroundColor: "#f3f1f1" }}>
+      <Box id="footer_top">
+        <Box>
+          <Text className="footer_top_category">PRODUCT OFFERING</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Flight</spam>, <spam>International Flight</spam>,
+            <spam>Charte Flight</spam>, <spam>Hotels</spam>,
+            <spam>International Hotels</spam>, <spam>Homestays and villas</spam>
+            ,<spam>holiday in india</spam>, <spam>International holiday</spam>,
+            <spam>Book hotel from UAE</spam>,
+            <spam>myBiz for Corporate Travel</spam>,
+            <spam>Book Online Cabs</spam>, <spam>Book Bus Tickets</spam>,
+            <spam>Book Train Tickets</spam>, <spam>Book Fights from US</spam>,
+            <spam>Trip Planner</spam>, <spam>One Way Cab</spam>,
+            <spam>Gift Card</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">MAKEMYTRIP</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>About Us</spam>, <spam>Investor Relation</spam>,
+            <spam>Career</spam>, <spam>MMT Foundation</spam>,
+            <spam>CSR Policy</spam>, <spam>myPartner-Travel Agent Portal</spam>,
+            <spam>Foreign Exchange</spam>, <spam>List your hotel</spam>,
+            <spam>Partners-Reabus</spam>, <spam>Partner-Goibibo</spam>,
+            <spam>Advertize with Us</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">ABOUT THE SITE</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Customer Support</spam>, <spam>Payment Security</spam>,
+            <spam>Privicy Policy</spam>, <spam>User Agreement</spam>,
+            <spam>Terms of Service</spam>, <spam>More Offices</spam>,
+            <spam>Make A Payment</spam>, <spam>Work From Home</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">TOP HOTELS IN INDIA</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Hotal Taj Mumbai</spam>, <spam>Hotel Oborai Mumbai</spam>
+            <spam>W Goa</spam>, <spam>Grand Hayat Goa</spam>,
+            <spam>The Westion Goa</spam>, <spam>Hotal Taj Mumbai</spam>,
+            <spam>Hotel Oborai Mumbai</spam>, <spam>W Goa</spam>,{" "}
+            <spam>Grand Hayat Goa</spam>, <spam>The Westion Goa</spam>,{" "}
+            <spam>Hotal Taj Mumbai</spam>,<spam>Hotel Oborai Mumbai</spam>,
+            <spam>W Goa</spam>, <spam>Grand Hayat Goa</spam>,
+            <spam>The Westion Goa</spam>, <spam>Hotal Taj Mumbai</spam>,
+            <spam>Hotel Oborai Mumbai</spam>, <spam>W Goa</spam>,{" "}
+            <spam>Grand Hayat Goa</spam>, <spam>The Westion Goa</spam>,{" "}
+            <spam>Hotal Taj Mumbai</spam>, <spam>Hotel Oborai Mumbai</spam>,
+            <spam>W Goa</spam>, <spam>Grand Hayat Goa</spam>,
+            <spam>The Westion Goa</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">TOP INTERNATIONAL HOTEL</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Adaaran Club Rannalhi</spam>, <spam>Burk Khalifa</spam>,
+            <spam>Trum International</spam>, <spam>Caesars Palace</spam>,
+            <spam>Taj Samudra Colombo</spam>, <spam>Adaaran Club Rannalhi</spam>
+            ,<spam>Burk Khalifa</spam>, <spam>Trum International</spam>,
+            <spam>Caesars Palace</spam>, <spam>Taj Samudra Colombo</spam>,
+            <spam>Adaaran Club Rannalhi</spam>, <spam>Burk Khalifa</spam>,
+            <spam>Trum International</spam>, <spam>Caesars Palace</spam>,
+            <spam>Taj Samudra Colombo</spam>, <spam>Adaaran Club Rannalhi</spam>
+            ,<spam>Burk Khalifa</spam>, <spam>Trum International</spam>,
+            <spam>Caesars Palace</spam>, <spam>Taj Samudra Colombo</spam>,
+            <spam>Adaaran Club Rannalhi</spam>, <spam>Burk Khalifa</spam>,
+            <spam>Trum International</spam>, <spam>Caesars Palace</spam>,
+            <spam>Taj Samudra Colombo</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">QUICK LINKS</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Customer Support</spam>, <spam>Payment Security</spam>,
+            <spam>Privicy Policy</spam>, <spam>User Agreement</spam>,
+            <spam>Terms of Service</spam>, <spam>More Offices</spam>,
+            <spam>Make A Payment</spam>, <spam>Work From Home</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">IMPORTANT LINKS</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>Flight</spam>, <spam>International Flight</spam>,
+            <spam>Charte Flight</spam>, <spam>Hotels</spam>,
+            <spam>International Hotels</spam>, <spam>Homestays and villas</spam>
+            ,<spam>holiday in india</spam>, <spam>International holiday</spam>,
+            <spam>Book hotel from UAE</spam>,
+            <spam>myBiz for Corporate Travel</spam>,
+            <spam>Book Online Cabs</spam>, <spam>Book Bus Tickets</spam>,
+            <spam>Book Train Tickets</spam>, <spam>Book Fights from US</spam>,
+            <spam>Trip Planner</spam>, <spam>One Way Cab</spam>,
+            <spam>Gift Card</spam>
+          </Text>
+        </Box>
+        <Box>
+          <Text className="footer_top_category">CORPORATE TRAVEL</Text>
+          <Text onClick={() => navigate("/")}>
+            <spam>About Us</spam>, <spam>Investor Relation</spam>,
+            <spam>Career</spam>, <spam>MMT Foundation</spam>,
+            <spam>CSR Policy</spam>, <spam>myPartner-Travel Agent Portal</spam>,
+            <spam>Foreign Exchange</spam>, <spam>List your hotel</spam>,
+            <spam>Partners-Reabus</spam>, <spam>Partner-Goibibo</spam>,
+            <spam>Advertize with Us</spam>
+          </Text>
+        </Box>
+      </Box>
+      <Box backgroundColor={"#f3f1f1"} width="100%">
+        <Box id="footer_mid">
+          <Flex gap={"20px"}>
+            <Box w={"32%"}>
+              <Text className="footer_about_me">Why PlanMyTrip?</Text>
+              <Text>
+                Established in 2000, PlanMyTrip has since positioned itself as
+                one of the leading companies, providing great offers,
+                competitive airfares, exclusive discounts, and a seamless online
+                booking experience to many of its customers. The experience of
+                booking your flight tickets, hotel stay, and holiday package
+                through our desktop site or mobile app can be done with complete
+                ease and no hassles at all. We also deliver amazing offers, such
+                as Instant Discounts, Fare Calendar, MyRewardsProgram, MyWallet,
+                and many more while updating them from time to time to better
+                suit our customers’ evolving needs and demands.
+              </Text>
+            </Box>
+            <Box w={"32%"}>
+              <Text className="footer_about_me">
+                Booking Flights with MakeMyTrip
+              </Text>
+              <Text>
+                At MakeMyTrip, you can find the best of deals and cheap air
+                tickets to any place you want by booking your tickets on our
+                website or app. Being India’s leading website for hotel, flight,
+                and holiday bookings, MakeMyTrip helps you book flight tickets
+                that are affordable and customized to your convenience. With
+                customer satisfaction being our ultimate goal, we also have a
+                24/7 dedicated helpline to cater to our customer’s queries and
+                concerns. Serving over 5 million happy customers, we at
+                MakeMyTrip are glad to fulfill the dreams of folks who need a
+                quick and easy means to find air tickets. You can get a hold of
+                the cheapest flight of your choice today while also enjoying the
+                other available options for your travel needs with us.
+              </Text>
+            </Box>
+            <Box w={"32%"}>
+              <Text className="footer_about_me">
+                Domestic Flights with MakeMyTrip
+              </Text>
+              <Text>
+                MakeMyTrip is India's leading player for flight bookings. With
+                the cheapest fare guarantee, experience great value at the
+                lowest price. Instant notifications ensure current flight
+                status, instant fare drops, amazing discounts, instant refunds
+                and rebook options, price comparisons and many more interesting
+                features.
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ planmytrip/src/Components/Navbar.jsx
@@ -0,0 +1,122 @@
+import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import "../App.css";
+import logo from "../Icon/Plan_my_trip.png";
+import flight from "../Icon/airplane.png";
+import hotel from "../Icon/hotels.png";
+import house from "../Icon/house.png";
+import holiday from "../Icon/holiday.png";
+import train from "../Icon/train.png";
+import bus from "../Icon/bus.png";
+import cab from "../Icon/cab.png";
+import forex from "../Icon/forex.png";
+import charted from "../Icon/charted.png";
+import activity from "../Icon/activities.png";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div
+        style={{
+          height: "60px",
+          boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          position: "fixed",
+          top: "0px",
+        }}
+      >
+        <Flex id="navbar" gap={"20px"}>
+          <Box
+            w={"10%"}
+            display="flex"
+            justifyContent={"left"}
+            alignItems="center"
+            cursor={"pointer"}
+            onClick={() => navigate("/")}
+          >
+            <Avatar src={logo} width={"100%"} />
+          </Box>
+          <Flex w={"60%"} id="nav_category" gap={"8px"}>
+            <Box className="nav_box" onClick={() => navigate("/flight")}>
+              <Avatar src={flight} className="nav_avatar" />
+              <Text className="nav_text">Flights</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/hotel")}>
+              <Avatar src={hotel} className="nav_avatar" />
+              <Text className="nav_text">Hotels</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/house")}>
+              <Avatar src={house} className="nav_avatar" />
+              <Text className="nav_text">Homestays</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/holiday")}>
+              <Avatar src={holiday} className="nav_avatar" />
+              <Text className="nav_text">HolidayPlan</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/train")}>
+              <Avatar src={train} className="nav_avatar" />
+              <Text className="nav_text">Train</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/bus")}>
+              <Avatar src={bus} className="nav_avatar" />
+              <Text className="nav_text">Buses</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/cab")}>
+              <Avatar src={cab} className="nav_avatar" />
+              <Text className="nav_text">Cabs</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/forex")}>
+              <Avatar src={forex} className="nav_avatar" />
+              <Text className="nav_text">Forex</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/charted")}>
+              <Avatar src={charted} className="nav_avatar" />
+              <Text className="nav_text">CharterPlane</Text>
+            </Box>
+            <Box className="nav_box" onClick={() => navigate("/activity")}>
+              <Avatar src={activity} className="nav_avatar" />
+              <Text className="nav_text">Activities</Text>
+            </Box>
+          </Flex>
+          <Flex w={"30%"} id="nav_filter">
+            <Box>
+              <Button
+                padding={"5px 10px"}
+                borderRadius="5px"
+                border={"0px"}
+                fontSize="17px"
+                backgroundColor={"#008cff"}
+                color="white"
+                onClick={() => navigate("/login")}
+                cursor="pointer"
+              >
+                Login
+              </Button>
+            </Box>
+            <Box>
+              <label className="select_label">Country</label>
+              <br />
+              <select className="select">
+                <option value="INDIA">INDIA</option>
+                <option value="UAE">UAE</option>
+                <option value="USA">USA</option>
+              </select>
+            </Box>
+            <Box>
+              <label className="select_label">Language</label>
+              <br />
+              <select className="select">
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Telugu">Telugu</option>
+              </select>
+            </Box>
+          </Flex>
+        </Flex>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
