da75d81b6d96eeb79307647e6ffafc6956de161e
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-01-20 10:57:43 +0530

    add day-3 file

--- planmytrip/src/App.js
+++ planmytrip/src/App.js
@@ -1,21 +1,15 @@
 
-
 import './App.css';
-import Home from './Home/Home';
-
-import logo from "./logo.svg";
+import Home from './Components/Home/Home';
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-
 
 function App() {
   return (
     <div className="App">
 
-      <Home/>
-
       <Navbar />
+      <Home/>
 
     </div>
   );
