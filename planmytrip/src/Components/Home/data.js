239c4bafca9eb957c9d745da51443e11be52d000
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-01-21 14:35:00 +0530

    add day-4 file

---
+++ planmytrip/src/Components/Home/data.js
@@ -0,0 +1,401 @@
+  export const data = [
+    {
+     "img":"https://promos.makemytrip.com/notification/xhdpi//Desktop_StampThatPassportSale_18Jan@2x.jpg",
+     "capText": "GENERAL",
+     "blackText": "LIVE NOW: Stamp-That-Passport Sale",
+     "font14": "for your international trips! Grab up to 35% OFF* on flights, hotels &amp; holiday packages.",
+     "font142": "BOOK NOW",
+     "capText 2": "INTL FLIGHTS",
+     "blackText 2": "Grab Up to ₹10,000 OFF* on International Flights",
+     "font14 3": "with Stamp-That-Passport Sale!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop_IF_STP_19Jan.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+     "img":"https://promos.makemytrip.com/notification/xhdpi//dh-Amex-116x116-31122021.jpg",
+     "capText": "INTL HOTELS",
+     "blackText": "Grab Up to 30% OFF* on International Hotels",
+     "font14": "with Stamp-That-Passport Sale!",
+     "font142": "BOOK NOW",
+     "capText 2": "HOLIDAYS",
+     "blackText 2": "Grab Up to 35% OFF* on International Holiday Packa...",
+     "font14 3": "with Stamp-That-Passport Sale!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//if-116x116-31122021.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//Amex-116x116-17082021.jpg",
+     "capText": "GENERAL",
+     "blackText": "PRESENTING: Republic of Travel Deals!",
+     "font14": "Grab up to 35% OFF* on flights, stays, holidays, cabs, buses &amp; trains for your unforgettable trips.",
+     "font142": "BOOK NOW",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "Grab Up to 25% OFF* on Domestic Flights",
+     "font14 3": "with our Republic of Travel Deals!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop_IntlHolidays_StampThatPassportSale_19Jan@2x.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//gofirst-116x116-11012023.jpg",
+     "capText": "DOM HOTELS",
+     "blackText": "Grab Up to 30% OFF* on Domestic Hotels",
+     "font14": "with our Republic of Travel Deals!",
+     "font142": "BOOK NOW",
+     "capText 2": "BUS",
+     "blackText 2": "Grab Up to 10% OFF* on Bus Tickets",
+     "font14 3": "with our Republic of Travel Deals!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//116X116-vistara-27112022.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//116x116-akasa-09012023.jpg",
+     "capText": "ALT ACCO",
+     "blackText": "Grab Up to 35% OFF* on Homestays",
+     "font14": "with our Republic of Travel Deals!",
+     "font142": "BOOK NOW",
+     "capText 2": "HOLIDAYS",
+     "blackText 2": "Grab Up to 35% OFF* on Holiday Packages",
+     "font14 3": "with our Republic of Travel Deals!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//116x116-Batik-10012023.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//spicejet-116x116-04012023.jpg",
+     "capText": "CABS",
+     "blackText": "Grab Up to 15% OFF* on Outstation Cabs",
+     "font14": "with our Republic of Travel Deals!",
+     "font142": "BOOK NOW",
+     "capText 2": "CABS",
+     "blackText 2": "Grab FLAT 5% OFF* on Airport Cabs",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//116x116-alliance-18112022.jpg",
+     "font14 3": "with our Republic of Travel Deals!",
+     "font14 4": "BOOK NOW"
+    },
+]
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//airasia-116x116-07072022.jpg",
+//      "capText": "CABS",
+//      "blackText": "Grab FLAT 10% OFF* on Hourly Rental Cabs",
+//      "font14": "with our Republic of Travel Deals!",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "JUST IN: SpiceJet’s Red Hot Sale!",
+//      "font14 3": "Book domestic flights starting @ JUST Rs. 1,766.",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop_IntlHotels_StampThatPassportSale_19Jan@2x.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//116X116-IH-HoneyMoon-21112022.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "Wow Flight Deals with AirAsia’s Flash Sale!",
+//      "font14": "Book domestic flights starting @ JUST Rs. 1,399.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "LIVE NOW: Go First’s Travel India Travel Sale!",
+//      "font14 3": "Book domestic flights starting @ JUST Rs. 1199*.",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//etihad-116x116-24112022.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//oyo-116x116-10112022.jpg",
+//      "capText": "INTL FLIGHTS",
+//      "blackText": "GRAB NOW: Huge Savings with IndiGo’s Flight Sale!",
+//      "font14": "Book international flights with additional discounts.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "INTL FLIGHTS",
+//      "blackText 2": "FOR YOU: Super Savings on Flights with Air India’s...",
+//      "font14 3": "Book international flights @ FLAT 5% OFF*",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//flybig-116x116-03012023.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//116X116-bajaj-03012023.jpg",
+//      "capText": "INTL FLIGHTS",
+//      "blackText": "LIVE NOW: Go First’s Travel India Travel Sale!",
+//      "font14": "Book international flights starting @ JUST Rs. 6599*.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "TAKING OFF NOW: New Flights by Vistara",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//Flights-insurance-116x116.jpg",
+//      "font14 3": "between Goa’s Mopa Airport &amp; major Indian cities!",
+//      "font14 4": "BOOK NOW"
+//     },
+// ]
+
+
+
+
+
+
+
+
+export const data2 = [
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//onecard-116x116-05012023.jpg",
+     "capText": "DOM FLIGHTS",
+     "blackText": "TAKING OFF NOW: New Flights by Go First",
+     "font14": "to select domestic destinations.",
+     "font142": "EXPLORE MORE",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "INTRODUCING: New Flights by Akasa Air",
+     "font14 3": "between Mumbai, Bengaluru and Goa!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//AncillaryMeals-01092022.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//116x116-ExtraBaggage-05092022.jpg",
+     "capText": "INTL FLIGHTS",
+     "blackText": "JUST IN: New Year Sale by Batik Air!",
+     "font14": "Book international flights starting @ JUST Rs. 24,121.",
+     "font142": "BOOK NOW",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "SpiceJet Just Launched Its New Flights",
+     "font14 3": "between Hyderabad, Jharsuguda &amp; Gwalior.",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//indigo-116x116-07072022.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//gofirst-116x116-07072022.jpg",
+     "capText": "DOM FLIGHTS",
+     "blackText": "INTRODUCED: New Flights by Alliance Air",
+     "font14": "between Bhubaneswar and Rourkela!",
+     "font142": "BOOK NOW",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "New Flights Introduced by AirAsia",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//Vh1Supersonic_DT_Banner_15Dec.jpg",
+     "font14 3": "between Delhi, Surat, Bengaluru &amp; Kolkata, starting @ JUST ₹4193*.",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//ita-airways-116x116-21122022.jpg",
+     "capText": "INTL HOTELS",
+     "blackText": "Love-filled Deal for Your Honeymoon: Up to 30% OFF...",
+     "font14": "on International Hotels. Have a romantic vacay!",
+     "font142": "BOOK NOW",
+     "capText 2": "INTL FLIGHTS",
+     "blackText 2": "Etihad Airways is BACK",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//116X116-vistara-23122022.jpg",
+     "font14 3": "with flights between Abu Dhabi-Kolkata and Abu Dhabi-New York!",
+     "font14 4": "EXPLORE MORE"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//akasa-116x116-18072022.jpg",
+     "capText": "DOM HOTELS",
+     "blackText": "Get FLAT 13% OFF* on Select OYO Hotels",
+     "font14": "&amp; enjoy comfortable stays.",
+     "font142": "BOOK NOW",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "New Flights Introduced by FlyBig",
+     "font14 3": "between Guwahati &amp; Itanagar!",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop_Pilgrimage_19July.jpg",
+     "font14 4": "BOOK NOW"
+    },
+    {
+        "img":"https://promos.makemytrip.com/notification/xhdpi//116X116-IF-CFAR-26092022.jpg",
+     "capText": "DOM FLIGHTS",
+     "blackText": "#SuperSaver Offer: FLAT 5% OFF* + No-Cost EMI",
+     "font14": "on domestic flights for your next trip!",
+     "font142": "Book Now",
+     "capText 2": "DOM FLIGHTS",
+     "blackText 2": "Get Your Journey Insured @ JUST Rs. 79*",
+     "font14 3": "and get claim of up to Rs. 3000*.",
+     "font14 4": "View Details",
+     "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop-Group-Features-191222.jpg",
+     "latoRegular": "Code: MMTBFLEMI"
+    },
+]
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//Desktop_IRCTC_30Dec@2x.jpg",
+//      "capText": "INTL HOTELS",
+//      "blackText": "FLAT 12% OFF* on International Hotels",
+//      "font14": "With SBI Credit Cards",
+//      "font142": "View Details",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "Meals for Your Flights @ up to 20% Lower Prices!",
+//      "font14 3": "Now book your meal in advance &amp; save BIG bucks on your journey.",
+//      "font14 4": "BOOK NOW",
+//      "latoRegular": "Code: MMTSBIINT",
+//      "img2":"https://promos.makemytrip.com/appfest/xhdpi//SUV_9JAN.jpg",
+//      "validTag": "Offer ends in 00h:12m"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//116x116-IH-Bali-28122022.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "Carry Extra Baggage @ 20%* Lower Prices!",
+//      "font14": "Now purchase EXCESS BAGGAGE allowance in advance &amp; save BIG bucks on your journey.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "TAKING OFF NOW: Flights by IndiGo",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//Desktop_Wildlife_22Aug@2x.jpg",
+//      "font14 3": "between Goa’s new Mopa Airport &amp; major Indian cities!",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//mobikwik-116x116-26122022.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "FLYING NOW: Go First Flights",
+//      "font14": "between Goa’s new Mopa Airport &amp; Mumbai\/Hyderabad\/Bengaluru!",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM HOTELS",
+//      "blackText 2": "FLAT 12% OFF* on Hotels, Villas &amp; Apartments",
+//      "font14 3": "With SBI Credit Cards",
+//      "font14 4": "View Details",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//desktop-Tripmoney-13122021.jpg",
+//      "latoRegular": "Code: MMTSBI",
+//      "validTag": "Offer ends in 00h:12m"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//116x116-bob-02012023.jpg",
+//      "capText": "HOLIDAYS",
+//      "blackText": "For Vh1 Supersonic, Pune:",
+//      "font14": "Up to 35% OFF* on holiday packages!",
+//      "font142": "BOOK NOW",
+//      "capText 2": "INTL FLIGHTS",
+//      "blackText 2": "JUST IN: ITA Airways, A New International Airline!",
+//      "font14 3": "Book a direct flight NOW between Delhi and Rome.",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+// [
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "FOR YOU: Special Discounts on Vistara Flights!",
+//      "font14": "Book domestic flights with up to Rs. 1,500 OFF*.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "Welcoming Akasa Air to the Indian Skies!",
+//      "font14 3": "Take off NOW with us in Akasa flights.",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "INTL FLIGHTS",
+//      "blackText": "INSIDE: BIG Discounts on Flights, Hotels, Holidays",
+//      "font14": "&amp; more, with SBI Credit Card for your next refreshing break.",
+//      "font142": "View Details",
+//      "capText 2": "HOLIDAYS",
+//      "blackText 2": "For Matches Made in Heaven:",
+//      "font14 3": "PERSONALISED HONEYMOON PACKAGES, incl. special experiences, starting @ JUST Rs. 29,000.",
+//      "font14 4": "EXPLORE PACKAGE",
+//      "latoRegular": "Code: SOULMATE",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "validTag": "Offer ends in 00h:12m"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "HOLIDAYS",
+//      "blackText": "For a Peaceful, Spiritual Break:",
+//      "font14": "Grab up to 30% OFF* on our specially-curated pilgrimage holiday packages.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "INTL FLIGHTS",
+//      "blackText 2": "INTRODUCED: ‘Zero Cancellation’!",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "font14 3": "A WOW benefit to help you get 100% refund on international flight cancellations.",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "DOM HOTELS",
+//      "blackText": "With the Group Booking Feature:",
+//      "font14": "Get up to 25% OFF* on booking minimum 6 hotel rooms.",
+//      "font142": "BOOK NOW",
+//      "capText 2": "INTL HOTELS",
+//      "blackText 2": "Grab Up to 30% OFF* on Bali Hotels",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "font14 3": "&amp; have the most rejuvenating trip ever!",
+//      "font14 4": "View Details"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "RAILS",
+//      "blackText": "To Book Your Train Journeys with Us:",
+//      "font14": "Sign up with IRCTC &amp; enjoy a hassle-free booking process!",
+//      "font142": "Explore Now",
+//      "capText 2": "CABS",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "blackText 2": "NEW: SUV Cabs for Group Airport Transfers!",
+//      "font14 3": "Book @ FLAT 5% OFF*.",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "HOLIDAYS",
+//      "blackText": "For a Thrilling Escape into the Wild:",
+//      "font14": "Grab up to 30% OFF* on our specially-curated wildlife holiday packages.",
+//      "font142": "EXPLORE PACKAGE",
+//      "capText 2": "GENERAL",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "blackText 2": "NEW: Easy Forex Solutions for Your Spends Abroad!",
+//      "font14 3": "Get best forex rates on foreign currency notes, FREE TripMoney Global Cash Card &amp; FREE BookMyForex Card.",
+//      "font14 4": "CLAIM NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "INTL FLIGHTS",
+//      "blackText": "Avail Interest FREE* EMI + Up to 30% OFF*",
+//      "font14": "on flights, hotels &amp; holiday packages for your trips in India &amp; abroad!",
+//      "font142": "Book Now",
+//      "capText 2": "DOM HOTELS",
+//      "blackText 2": "50th Anniversary-Special Deal: Up to 50% OFF*",
+//      "font14 3": "on Royal Orchid Hotels. Have a delightful stay!",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "font14 4": "BOOK NOW"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "DOM HOTELS",
+//      "blackText": "Grab FLAT 15% OFF* on Domestic Hotels, Homestays!",
+//      "font14": "And enjoy BIG savings on the stay of your dreams.",
+//      "font142": "View Details",
+//      "capText 2": "INTL FLIGHTS",
+//      "blackText 2": "Grab FLAT 10% OFF* on International Flights with A...",
+//      "font14 3": "with American Express Cards.",
+//      "font14 4": "View Details",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "latoRegular": "Code: MMTAMEX",
+//      "validTag": "Offer starts in 00h:12m"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "Grab NOW: FLAT 12% OFF* on Domestic Flights!",
+//      "font14": "Valid on ICICI Bank Credit Cards, Credit Cards EMI &amp; Debit Cards",
+//      "font142": "View Details",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "Grab FLAT 15% OFF* on Domestic Flights",
+//      "font14 3": "with American Express Cards.",
+//      "font14 4": "View Details",
+//      "latoRegular": "Code: FLYMON",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "validTag": "Offer starts on 23 Jan 2023"
+//     },
+//     {
+//         "img":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//      "capText": "DOM FLIGHTS",
+//      "blackText": "Flat 12% Instant Discount on Domestic Flights!",
+//      "font14": "Valid Only on Citi Credit and Debit cards.",
+//      "font142": "View Details",
+//      "capText 2": "DOM FLIGHTS",
+//      "blackText 2": "FLAT 12% OFF* on Domestic Flights!",
+//      "font14 3": "Valid on Axis Bank Credit &amp; Debit Cards",
+//      "font14 4": "View Details",
+//      "latoRegular": "Code: FLYNOW",
+//      "validTag": "Offer starts on 24 Jan 2023",
+//      "img2":"https://promos.makemytrip.com/notification/xhdpi//RoyalOrchid-116x116-16012023.jpg",
+//     }
+// ]
