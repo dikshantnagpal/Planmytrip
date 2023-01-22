93617a0418bb9eb87970b83acc1c546c483e347b
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-01-22 10:59:29 +0530

    add day-5 file

--- planmytrip/src/App.js
+++ planmytrip/src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 import Home from "./Components/Home/Home";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { ImageCard } from "./Components/Home/ImageCard";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
       <Home/>
-
+      <Footer/>
     </div>
   );
 }
