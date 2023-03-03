d2a057179666042bfd086f430279777d4ba12c3d
vikram <vk14133m02059@gmail.com>
2023-03-03 20:24:04 +0530

    Added Single Product page

--- planmytrip/src/Components/MainRoutes.jsx
+++ planmytrip/src/Components/MainRoutes.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Home from "./Home/Home"
-import Login from './Page/Login'
-import { SignUp } from './Page/SignUp'
-import { UserAuthContextProvider } from './Context/UserAuthContext'
-import Hotels from './Hotels/Hotels'
-import { Delhi } from './Cities/Delhi'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Login from "./Page/Login";
+import { SignUp } from "./Page/SignUp";
+import { UserAuthContextProvider } from "./Context/UserAuthContext";
+import Hotels from "./Hotels/Hotels";
+import { Delhi } from "./Cities/Delhi";
+import SinglePage from "../Pages/SinglePage";
 const MainRoutes = () => {
   return (
     <div>
       <UserAuthContextProvider>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/hotels" element={<Hotels/>}/>
-          <Route path="/delhi" element={<Delhi/>} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/delhi" element={<Delhi />} />
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/delhi/:id" element={<SinglePage />}></Route>
         </Routes>
       </UserAuthContextProvider>
-        
     </div>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
